faf6f4bbb345beeb812363f5ef503d677edd5177
DenissR <deniss777@live.ru>
2023-01-11 00:31:27 +0200

    Trying method sout

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Deniss");
+        System .out.println(30 + 12);
+        System.out.println("From Tallinn");
+        System.out.println("     +     ");
+        System.out.println("    +++    ");
+        System.out.println("   +++++   ");
+        System.out.println("  +++++++  ");
+        System.out.println(" +++++++++ ");
+        System.out.println("+++++++++++");
+    }
+}
